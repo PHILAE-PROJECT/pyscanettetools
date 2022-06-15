088e2acc1b56cdda6659ef06a46ac7ebab760935
ftamagna <frederic.tamagnan@univ-fcomte.fr>
2022-06-15 16:36:20 +0200

    refactored examples

--- example_executability.py
+++ example_executability.py
@@ -1,5 +1,4 @@
 from pyscanettetools.samplequality import executability,coverage
-from pyscanettetools.generation import generation
 
 traceset_grouped=[['debloquer_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'scanner_0', 'transmission_0', 'abandon_?', 'payer_1'],['ajouter_0']]
 print(len(traceset_grouped))
@@ -7,8 +6,7 @@ print(executability(traceset_grouped))
 print(coverage(traceset_grouped))
 from pyscanettetools.utils_example import read_traces_csv
 from pathlib import Path
-traceset = read_traces_csv(Path("../pyscanettetools/csv/1026-steps.csv"))
+traceset = read_traces_csv(Path("pyscanettetools/csv/1026-steps.csv"))
 traceset2 = traceset.with_traces_grouped_by("sessionID", property=True)
 print(executability(traceset2))
 coverage(traceset2)
-generation(path="../pyscanettetools/csv/hello.csv", number_of_sessions=150)
---
+++ examplegeneration.py
@@ -0,0 +1,2 @@
+from pyscanettetools.generation import generation
+generation(path="pyscanettetools/csv/hello.csv", number_of_sessions=150)
